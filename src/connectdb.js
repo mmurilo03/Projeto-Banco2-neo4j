a2fbf560baddb21cabc9902b04589aa4273d76af
Elivelton Pereira <elivelton.pereira@academico.ifpb.edu.br>
2023-07-09 11:16:15 -0300

    Criando e estilizando autenticação de usuário

--- src/connectdb.js
+++ src/connectdb.js
@@ -2,21 +2,10 @@ const express = require("express")
 const cors = require("cors")
 const app = express()
 app.use(express.json())
-const port = 3001
+const port = 3000
 const PointRouter = require("../routes/PointRouter")
 const UserRouter = require("../routes/UserRouter")
-const session = require("express-session");
-const connectSqlite = require("connect-sqlite3")(session);
-app.use(
-    session({
-      secret: process.env.SECRET,
-      saveUninitialized: true,
-      resave: true,
-      name: "userSession",
-      cookie: { maxAge: 86400000 }, //1 dia
-      store: new connectSqlite(),
-    })
-  );
+
 
 app.use(cors())
 app.use("/pontos", PointRouter)
